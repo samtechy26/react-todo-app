cc139f7e33718e926adf5d0a15bbf1357adeaa02
Sam Akan <samakan061@gmail.com>
2022-10-03 19:47:28 +0300

    publishing my first react app

---
+++ src/components/pages/About.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { useMatch, Link, Route  } from "react-router-dom"
+import SinglePage from "./SinglePage"
+
+const About = () => {
+  const { url, path } = useMatch()
+
+return (
+  <div>
+    <ul>
+      <li>
+        <Link to={`${url}/about-app`}>About App</Link>
+      </li>
+      <li>
+        <Link to={`${url}/about-author`}>About Author</Link>
+      </li>
+    </ul>
+    <Route path={`${path}/:slug`} element={<SinglePage />} />
+      
+    
+  </div>
+)
+}
+export default About
